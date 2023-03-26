9d9ab4763794c906308fcd3002db12862f6dd950
jasim <mdjasimuddimruet@gmail.com>
2023-03-27 00:23:50 +0600

    Strict mode in Javascript

---
+++ script.js
@@ -0,0 +1,113 @@
+// console.log('This is the first line code for testing js file');
+// for strict mode 
+//we use 'use strict';
+
+
+//otherwise for non strict mode sometimes call sloppy mood
+
+
+//use strict mode in whole code
+
+// 'use strict'
+
+// var name = 'Md Jasim Uddim';
+// var age = 25;
+
+//  address = 'Rajshahi';
+
+// console.log('My name is ' + name + ' and I am ' + age + ' years old!');
+
+// function aFunc() {
+//    job = 'Student';
+//    console.log('A Function with Strict Mode Enabled');
+// }
+
+// aFunc();
+
+// console.log('I am a ' + job + ' And I am from ' + address);
+
+//use strict mode in some code 
+
+
+
+// var name = 'Md Jasim Uddim';
+// var age = 25;
+
+//  address = 'Rajshahi';
+
+// console.log('My name is ' + name + ' and I am ' + age + ' years old!');
+
+// function aFunc() {
+//     'use strict';
+//    job = 'Student';
+//    console.log('A Function with Strict Mode Enabled');
+// }
+
+// aFunc();
+
+// console.log('I am a ' + job + ' And I am from ' + address);
+
+
+
+//for sloppy mode
+// var name = 'Zonayed Ahmed';
+// var age = 10;
+
+// address = 'Cumilla';
+
+// console.log('My name is ' + name + ' and I am ' + age + ' years old!');
+
+// function aFunc() {
+//    job = 'Student';
+//    console.log('A Function with Strict Mode Enabled');
+// }
+
+// aFunc();
+
+// console.log('I am a ' + job + ' And I am from ' + address);
+
+
+// var name = 'Zonayed Ahmed';
+// nama = 'Ahmed Zonayed';
+// console.log(name);
+
+// 'use strict'
+// var name = 'Zonayed Ahmed';
+// nama = 'Ahmed Zonayed';
+// console.log(name);
+
+
+//without strict mode
+// Existing Object
+// var obj = {
+//     name: 'Zonayed Ahmed',
+//     job: 'Student'
+// }
+
+// // obj.age = 25;
+// // console.log(obj.age);
+// Object.preventExtensions(obj);
+
+// // // Assign a new properties
+// obj.age = 35;
+
+// // // Try to print this new one
+// console.log(obj.age);
+
+//using strict mode
+// Existing Object
+// 'use strict';
+// var obj = {
+//     name: 'Zonayed Ahmed',
+//     job: 'Student'
+// }
+
+// // obj.age = 25;
+// // console.log(obj.age);
+// Object.preventExtensions(obj);
+
+// // // Assign a new properties
+// obj.age = 35;
+
+// // // Try to print this new one
+// console.log(obj.age);
